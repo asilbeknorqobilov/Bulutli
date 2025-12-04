52121f4edfaa8fa864f85c19ff4cd550af2cf707
HP <asilbeknorqobilov88@gmail.com>
2025-12-04 12:21:15 +0500

    Initial Create

---
+++ Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static string Ikkilik(int n)
+    {
+        if (n == 0) return "0";
+
+        string result = "";
+
+        while (n > 0)
+        {
+            int qoldiq = n % 2;
+            result = qoldiq + result;
+            n /= 2;
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine(Ikkilik(10));
+    }
+}
