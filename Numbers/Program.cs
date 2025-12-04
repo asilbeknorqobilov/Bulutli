6ba2e6ec316d347c0ab30b11a71397fd3807953e
HP <asilbeknorqobilov88@gmail.com>
2025-12-04 12:27:11 +0500

    Sakkizlik

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -18,6 +18,20 @@ class Program
         return result;
     }
 
+    
+    static string Sakkizlik(int n)
+    {
+        if (n == 0) return "0";
+        string result = "";
+        while (n > 0)
+        {
+            int remainder = n % 8;
+            result = remainder + result;
+            n /= 8;
+        }
+        return result;
+    }
+    
     static void Main(string[] args)
     {
 
